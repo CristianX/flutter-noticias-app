3835fe90ba30871986f23f42b0b819b7b3b03c47
CristianX <thecristianx@hotmail.com>
2020-05-27 17:46:58 -0500

    Implementando menú de navegación bottomNavigationBar

--- lib/src/pages/tabs_page.dart
+++ lib/src/pages/tabs_page.dart
@@ -5,21 +5,47 @@ class TabsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
 
-      body: PageView(
-        // Eliminando indicación de que ya no hay más scroll
-        physics: BouncingScrollPhysics(),
-        children: <Widget>[
-          Container(
-            color: Colors.red
-          ),
-
-          Container(
-            color: Colors.green,
-          )
-
-        ],
-      )
+      body: _Paginas(),
+      bottomNavigationBar: _Navegacion(),
       
+    );
+  }
+}
+
+class _Navegacion extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // BottomNavigationBar necesita por lo menos 2 items para funcionar
+    return BottomNavigationBar(
+      // Item seleccionado por default
+      currentIndex: 0,
+      items: [
+        BottomNavigationBarItem( icon: Icon( Icons.person_outline ), title: Text( 'Para ti' ) ),
+        BottomNavigationBarItem( icon: Icon( Icons.public ), title: Text( 'Encabezados' ) ),
+        
+      ]
+    );
+  }
+}
+
+class _Paginas extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      // Eliminando indicación de que ya no hay más scroll
+      // physics: BouncingScrollPhysics(),
+      // Evitando deslizamiento
+      physics: NeverScrollableScrollPhysics(),
+      children: <Widget>[
+        Container(
+          color: Colors.red
+        ),
+
+        Container(
+          color: Colors.green,
+        )
+
+      ],
     );
   }
 }
