9844e994eefb3eab813e890cff55eeda1b0f8f60
CristianX <thecristianx@hotmail.com>
2020-05-27 18:11:43 -0500

    Implementando provider como manejador de estado

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 A new Flutter project.
 
+## Dependencias
+* Provider `provider: ^4.1.2`
+
 ## Getting Started
 
 This project is a starting point for a Flutter application.
--- lib/src/pages/tabs_page.dart
+++ lib/src/pages/tabs_page.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 
+// Paquete provider
+import 'package:provider/provider.dart';
+
 class TabsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return ChangeNotifierProvider(
+      // Instancia global
+      create: (_) => new _NavegacionModel(),
+      child: Scaffold(
 
-      body: _Paginas(),
-      bottomNavigationBar: _Navegacion(),
-      
+        body: _Paginas(),
+        bottomNavigationBar: _Navegacion(),
+        
+      ),
     );
   }
 }
@@ -15,10 +22,16 @@ class TabsPage extends StatelessWidget {
 class _Navegacion extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    // Obteniendo instancia de clase como que fuera un Singleton
+    final navegacionModel = Provider.of<_NavegacionModel>(context);
+
     // BottomNavigationBar necesita por lo menos 2 items para funcionar
     return BottomNavigationBar(
       // Item seleccionado por default
-      currentIndex: 0,
+      currentIndex: navegacionModel.paginaActual,
+      // para delvolver el indice
+      onTap: (i) => navegacionModel.paginaActual = i,
       items: [
         BottomNavigationBarItem( icon: Icon( Icons.person_outline ), title: Text( 'Para ti' ) ),
         BottomNavigationBarItem( icon: Icon( Icons.public ), title: Text( 'Encabezados' ) ),
@@ -48,4 +61,23 @@ class _Paginas extends StatelessWidget {
       ],
     );
   }
+}
+
+
+class _NavegacionModel with ChangeNotifier{
+
+  int _paginaActual = 0;
+
+  // Getters
+  int get paginaActual => this._paginaActual;
+
+  // Setter
+  set paginaActual( int valor ) {
+    this._paginaActual = valor;
+
+    // Para que todos los widgets esten pendientes de ese valor
+    notifyListeners();
+  }
+
+
 }
