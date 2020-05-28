61ff7864665c04027560002c25465759a18f4b47
CristianX <thecristianx@hotmail.com>
2020-05-27 21:14:07 -0500

    Diseñando widget de noticias

--- lib/src/widgets/lista_noticias.dart
+++ lib/src/widgets/lista_noticias.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 // Modelos
 import 'package:news_app/src/models/news_models.dart';
+import 'package:news_app/src/theme/tema.dart';
 
 class ListaNoticias extends StatelessWidget {
 
@@ -15,8 +16,88 @@ class ListaNoticias extends StatelessWidget {
     return ListView.builder(
       itemCount: this.noticias.length,
       itemBuilder: (BuildContext context, int index) {
-      return Text( this.noticias[index].title);
+        return _Noticia( noticia: this.noticias[index], index: index );
      },
+    );
+  }
+}
+
+
+class _Noticia extends StatelessWidget {
+
+  final Article noticia;
+  final int index;
+
+  _Noticia({
+    @required this.noticia, 
+    @required this.index
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        _TarjetaTopbar( noticia, index ),
+
+        _TarjetaTitulo( noticia ),
+
+        _TarjetaImagen( noticia )
+      ],
+    );
+  }
+}
+
+class _TarjetaTopbar extends StatelessWidget {
+  
+  final Article noticia;
+  final int index;
+
+  const _TarjetaTopbar( this.noticia, this.index );
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding : EdgeInsets.symmetric( horizontal: 10.0 ),
+      margin  : EdgeInsets.only( bottom: 10.0 ),
+      child: Row(
+        children: <Widget>[
+          Text('${ index + 1 }. ', style: TextStyle( color: miTema.accentColor )),
+          Text('${ noticia.source.name }. ')
+        ],
+      ),
+      
+    );
+  }
+}
+
+class _TarjetaTitulo extends StatelessWidget {
+
+  final Article noticia;
+
+  const _TarjetaTitulo(this.noticia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric( horizontal: 15 ),
+      child: Text( noticia.title, style: TextStyle( fontSize: 20, fontWeight: FontWeight.w700 ) ),
+      
+    );
+  }
+}
+
+class _TarjetaImagen extends StatelessWidget {
+
+  final Article noticia;
+
+  const _TarjetaImagen(this.noticia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+      child: Text('Hola mundo'),
+      
     );
   }
 }
