4c1ef59b2fd3e9ecb7bbd86b0c910724a123327f
CristianX <thecristianx@hotmail.com>
2020-05-27 19:49:34 -0500

    Cambiando el pageView según el elemento seleccionado

--- lib/src/pages/tabs_page.dart
+++ lib/src/pages/tabs_page.dart
@@ -44,7 +44,14 @@ class _Navegacion extends StatelessWidget {
 class _Paginas extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    // Instancia de pageController
+    final navegacionModel = Provider.of<_NavegacionModel>(context);
+
     return PageView(
+      // Controlando las páginas con el getter del pageController
+      controller: navegacionModel.pageController,
+
       // Eliminando indicación de que ya no hay más scroll
       // physics: BouncingScrollPhysics(),
       // Evitando deslizamiento
@@ -68,16 +75,21 @@ class _NavegacionModel with ChangeNotifier{
 
   int _paginaActual = 0;
 
+  PageController _pageController = new PageController();
+
   // Getters
   int get paginaActual => this._paginaActual;
 
   // Setter
   set paginaActual( int valor ) {
     this._paginaActual = valor;
+    _pageController.animateToPage(valor, duration: Duration( milliseconds: 250 ), curve: Curves.easeInOut);
 
     // Para que todos los widgets esten pendientes de ese valor
     notifyListeners();
   }
 
+  // Getter de _pageController
+  PageController get pageController => this._pageController;
 
 }
